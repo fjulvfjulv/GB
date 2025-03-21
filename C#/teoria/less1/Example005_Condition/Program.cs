18303fab57729f8bc4bb43b2cc64ac8acca010f1
Iuliia <fjulvfjulv@gmail.com>
2025-03-21 10:45:29 +0100

    Done

---
+++ C#/teoria/less1/Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dima")
+{
+    Console.WriteLine("Привет, пингвин!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
---
+++ C#/teoria/less1/Example007_Magic/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Clear();
+
+int xa = 1, ya = 1;
+int xb = 1, yb = 30;
+int xc = 80, yc = 30;
+
+// Проверяем, не выходит ли курсор за пределы консоли
+int maxWidth = Console.BufferWidth - 1;
+int maxHeight = Console.BufferHeight - 1;
+
+// Корректируем координаты, если они выходят за пределы
+ya = Math.Min(ya, maxHeight);
+yb = Math.Min(yb, maxHeight);
+yc = Math.Min(yc, maxHeight);
+
+xa = Math.Min(xa, maxWidth);
+xb = Math.Min(xb, maxWidth);
+xc = Math.Min(xc, maxWidth);
+
+// Устанавливаем курсор и выводим символы
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count < 1000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
+
+
+
+// Ожидание, чтобы консоль не закрылась сразу
+Console.ReadKey();
