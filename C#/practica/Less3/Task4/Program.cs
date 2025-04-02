57e64cc28897d9eb7879c03177a2e2aca6fdb910
Iuliia <fjulvfjulv@gmail.com>
2025-04-02 14:13:20 +0200

    Added part of the homework/less3

---
+++ C#/practica/Less3/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая генерирует последовательность случайных чисел 
+// из 10 элементов в диапазоне от 1 до 10, и подсчитывает, сколько сгенерировалось чисел больше 5.
+
+
+int[] GenerateArray()
+{
+    int[] array = new int[10];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 11);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int More5(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 5) count++;
+    }
+    return count;
+}
+
+int[] array = GenerateArray();
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine(More5(array));
---
+++ C#/practica/Less3/Task5/Program.cs
@@ -0,0 +1,74 @@
+﻿// Напишите программу, которая выводит первые заданные элемента следующей последовательности:
+// -> 12
+// 1 2 2 3 3 3 4 4 4 4 5 5
+
+int RequestNumber(string msg)
+{
+    Console.WriteLine($"{msg} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsFull(int[] array)
+{
+    foreach (int item in array)
+    {
+        if (item == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int[] GenerateSequenceOfNumbers(int number)
+{
+    int[] array = new int[number];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == 0)
+        {
+            count++;
+            for (int j = 0; j <= i; j++)
+            {
+                if (!(IsFull(array))) array[i + j] = count;
+            }
+        }
+    }
+    return array;
+}
+
+void Generate(int number)
+{
+    int i = 1;
+    int count = 0;
+
+    while (i < number)
+    {
+        while (count < number)
+        {
+            int j = 0;
+            while (j < i)
+            {
+                System.Console.Write($"{i} ");
+                j++;
+            }
+            i++;
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int num = RequestNumber("Enter your number:");
+Generate(num);
+// PrintArray(arr);
