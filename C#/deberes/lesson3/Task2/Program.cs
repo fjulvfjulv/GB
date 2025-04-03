6be2b191aef63f292570d7852575b28179ab73d2
Iuliia <fjulvfjulv@gmail.com>
2025-04-03 13:12:15 +0200

    Added homework3

---
+++ C#/deberes/lesson3/Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double RequestNumber(string msg)
+{
+    Console.WriteLine($"{msg} ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double[] RequestPoints()
+{
+    double[] points = new double[6];
+    {
+        for (int i = 1; i < 3; i++)
+        {
+            points[i - 1] = RequestNumber($"Enter coordinate X for the point {i}");
+        }
+        for (int i = 1; i < 3; i++)
+        {
+            points[i + 1] = RequestNumber($"Enter coordinate Y for the point {i}");
+        }
+        for (int i = 1; i < 3; i++)
+        {
+            points[i + 3] = RequestNumber($"Enter coordinate Z for the point {i}");
+        }
+    }
+    return points;
+}
+
+
+double[] FindDiference(double[] points)
+{
+    double[] diference = new double[3];
+    diference[0] = points[1] - points[0];
+    diference[1] = points[3] - points[2];
+    diference[2] = points[5] - points[4];
+
+    return diference;
+}
+
+double FindDistance(double[] points)
+{
+    double[] dif = FindDiference(points);
+    double sum = 0;
+    for (int i = 0; i < dif.Length; i++)
+    {
+        sum += dif[i] * dif[i];
+    }
+
+    return Math.Sqrt(sum);
+}
+
+double[] points = RequestPoints();
+double dist = FindDistance(points);
+System.Console.WriteLine(dist);
+
+
+
+
---
+++ C#/deberes/lesson3/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int RequestNumber(string msg)
+{
+    Console.WriteLine($"{msg} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ModulOfNumber(int num)
+{
+    if (num < 0) return -num;
+    return num;
+}
+
+void CreateCubes(int num)
+{
+    for (int i = 1; i <= ModulOfNumber(num); i++)
+    {
+        if (num < 0)
+        {
+            Console.Write($"{-i * i * i}  ");
+            }
+        else
+        {
+            Console.Write($"{i * i * i}  ");
+            }
+    }
+}
+
+int number = RequestNumber("Enter your number");
+CreateCubes(number);
